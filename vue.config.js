ee52b7d6efac5906ffb0e48c84e394d83c1a1515
Heru Joko Utomo <heru@wallextech.com>
2020-04-19 13:11:53 +0800

    ignore moment locale

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,9 @@ let optimization = {};
 // comment line 6 to disable analyzer
 plugins.push(new BundleAnalyzerPlugin());
 
+// ignore moment locale
+plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
+
 module.exports = {
     lintOnSave: false,
     configureWebpack: {
